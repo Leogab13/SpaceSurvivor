307bfdebc7a068a2c12d3aef62b51c94f6a2c985
Padnzk <patri.97.60@outlook.it>
2021-11-11 23:01:00 +0100

    fixed ship controller
    ora la nave si ferma ai bordi

--- SpaceProject/Assets/Scripts/movimentoNave.cs
+++ SpaceProject/Assets/Scripts/movimentoNave.cs
@@ -4,8 +4,8 @@ using UnityEngine;
 
 public class movimentoNave : MonoBehaviour
 {
-    Rigidbody2D rb;
-    public float speed = 100;
+    Rigidbody2D rb;              //corpo nave
+    public float speed = 100;    //velocità nave
 
     // Start is called before the first frame update
     void Start()
@@ -14,12 +14,38 @@ public class movimentoNave : MonoBehaviour
     }
 
     // Update is called once per frame
-    void FixedUpdate()
+
+  
+
+    void FixedUpdate()      //con il fixed update controllo i movimenti della nave
     {
 
-        float asseX = Input.GetAxisRaw("Horizontal") * Time.deltaTime * speed;
+        if (transform.position.x >= -1.56 && transform.position.x <= 1.56)
+        {
 
-        rb.velocity = new Vector2(asseX,0);
+            float asseX = Input.GetAxisRaw("Horizontal") * Time.deltaTime * speed;
+
+            rb.velocity = new Vector2(asseX, 0);
+        }
+
+       
 
     }
+
+    void Update()        //codice per limitare lo spostamento della nave al range della telecamera
+    {
+        if (transform.position.x < -1.56f)
+        {
+            transform.position = new Vector2(-1.55f, -2.5f);             //a little teleport (shh) #1
+        }
+
+        if (transform.position.x > 1.56f)
+        {
+            transform.position = new Vector2(1.55f, -2.5f);             //a little teleport (shh) #2
+        }
+
+
+    }
+
+
 }
