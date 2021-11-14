977e33c67cca79876f3076b44d4dbd464ec23de0
Leogab13 <93938052+Leogab13@users.noreply.github.com>
2021-11-14 23:38:01 +0100

    Improved MoveShip script
    Cambiato lo script di movimento della nave togliendo l'uso della fisica e impedendo il movimento oltre la telecamera invece che teletrasportare la nave quando supera i confini

--- SpaceProject/Assets/Scripts/moveShip.cs
+++ SpaceProject/Assets/Scripts/moveShip.cs
@@ -5,46 +5,35 @@ using UnityEngine;
 public class moveShip : MonoBehaviour
 {
     Rigidbody2D rb;              //corpo nave
-    public float speed = 100;    //velocità nave
+    public float speed = 3.0f;    //velocità nave
+    Animator animator;
+    float asseX;
 
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+    }
+
+     void FixedUpdate()      //con il fixed update controllo i movimenti della nave
+    {
+        Vector2 position = rb.position;
+
+        if (!(position.x <= -1.56 && asseX < 0)&&!(position.x >= 1.56 && asseX > 0))//se non sto uscendo dai margini della telecamera
+        {
+            position.x = position.x + asseX * Time.deltaTime * speed;
+            rb.MovePosition(position);
+        }
+
+
+
     }
 
     // Update is called once per frame
-
-  
-
-    void FixedUpdate()      //con il fixed update controllo i movimenti della nave
+    void Update()        
     {
-
-        if (transform.position.x >= -1.56 && transform.position.x <= 1.56)
-        {
-
-            float asseX = Input.GetAxisRaw("Horizontal") * Time.deltaTime * speed;
-
-            rb.velocity = new Vector2(asseX, 0);
-        }
-
-       
-
-    }
-
-    void Update()        //codice per limitare lo spostamento della nave al range della telecamera
-    {
-        if (transform.position.x < -1.56f)
-        {
-            transform.position = new Vector2(-1.55f, -2.5f);             //a little teleport (shh) #1
-        }
-
-        if (transform.position.x > 1.56f)
-        {
-            transform.position = new Vector2(1.55f, -2.5f);             //a little teleport (shh) #2
-        }
-
-
+        asseX = Input.GetAxisRaw("Horizontal"); //leggo l'input        
     }
 
 
