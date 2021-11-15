3baf34fd24f4ec8f691d2166b93cac0d2a9fe7f1
Padnzk <patri.97.60@outlook.it>
2021-11-15 13:59:52 +0100

    Aggiunto punteggio

---
+++ SpaceProject/Assets/punteggio.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class punteggio : MonoBehaviour
+{
+    public static int score;
+    // Start is called before the first frame update
+    void Start()
+    {
+        score = 0;   
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        GetComponent<Text>().text = score.ToString();
+
+        score = score + 10;
+    }
+}
