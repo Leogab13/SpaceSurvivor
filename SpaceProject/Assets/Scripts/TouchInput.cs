6b808e61949d610f1da1da435b86b5a68f75abe2
Leogab13 <93938052+Leogab13@users.noreply.github.com>
2021-12-12 17:26:22 +0100

    Started writing TouchInput
    Bug noto: il gruppo bonus con HealthCollectible e ShieldCollectible a volte appare invisibile
    Bug noto: se si ruota il telefono il gioco va in modalità landscape
    TouchInput non testato e non collegato a ShipController

---
+++ SpaceProject/Assets/Scripts/TouchInput.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TouchInput : MonoBehaviour    
+{
+    Rect left;
+    Rect right;
+    Touch touch;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        left = new Rect(0, 0, Screen.width / 2, Screen.height);
+        right = new Rect(Screen.width / 2, 0, Screen.width / 2, Screen.height);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        touch = Input.GetTouch(0);
+
+        if (Input.touchCount > 0)
+        {
+            if (left.Contains(touch.position))
+            {
+                Debug.Log("left touched");
+            }
+            if (right.Contains(touch.position))
+            {
+                Debug.Log("right touched");
+            }
+        }
+    }
+}
