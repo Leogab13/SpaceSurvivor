cd178c5cff7d93ec0b6b24b79673dec234f3db7a
Leogab13 <93938052+Leogab13@users.noreply.github.com>
2022-01-02 16:59:57 +0100

    Added RedCoin animation, changed back TouchInput to Update instead of FixedUpdate, renamed prefab groups

--- SpaceProject/Assets/Scripts/TouchInput.cs
+++ SpaceProject/Assets/Scripts/TouchInput.cs
@@ -19,7 +19,7 @@ public class TouchInput : MonoBehaviour
     }
 
     // Update is called once per frame
-    void FixedUpdate()
+    void Update()
     {
         if (Input.touchCount > 0)
         {
